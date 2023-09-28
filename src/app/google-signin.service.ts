18458df55ee9b25c07b652b6bdf0c34afb54e4d1
Alina Shtramwasser <Alina.Shtramwasser@computershare.com>
2023-09-28 12:34:51 -0400

    completed tutorial

--- src/app/google-signin.service.ts
+++ src/app/google-signin.service.ts
@@ -7,6 +7,9 @@ import { ActivatedRoute } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
+
+// for an example: https://www.youtube.com/watch?v=G4BBNq1tgwE in particular
+// https://github.com/israelquiroz93/JWTClientYT/blob/master/src/app/services/auth.service.ts
 export class GoogleSigninService {
   //the path and client id can both go into an environment file so that ppl can't see them
   path: string | undefined;
@@ -23,16 +26,22 @@ export class GoogleSigninService {
     console.log("token deleted");
   }
 
-  logInWithGoogle(credentials: string): Observable<any> {
-    this.path = window.location.href;
+  logInWithGoogle(credential: string): Observable<any> {
+    //debugger;
+    //this.baseUrl = "http://localhost:12895/api/winery/";
+    this.path = "http://localhost:12895/LoginWithGoogle/"; //window.location.href;//(should be "https://localhost:12895/")
     const header = new HttpHeaders().set('Content-type', "application/json");
+    //this works but doesn't post it anywhere
+    /*
     const obs = new Observable((observer) => {
         observer.next(credentials)
     })
+    return obs;
+    */
     //will have to post it and parse it somewhere so I think it should go to the api that's gonna do that
     //need to watch tutorial meanwhile returning an observable based on tutorial
     //observable tutorial: https://www.tektutorialshub.com/angular/angular-observable-tutorial-using-rxjs/
-    return obs;
-    //return this.http.post(this.path, JSON.stringify(credentials), { headers: header });
+    
+    return this.http.post(this.path, JSON.stringify(credential), { headers: header, withCredentials: true });
   }
 }
